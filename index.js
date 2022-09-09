0bf6bdd31823b00e04d7a7573241626e6a641964
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-09-09 09:01:50 +0300

    add file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json())
+  .then(json => renderBooks(json));
   
 }
 
@@ -11,6 +14,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+//console.log(renderBooks())
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
